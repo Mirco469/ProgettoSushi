1800559a21254be0027b73fed68378b9544dd0c5
Alessandro Dindinelli <48436693+aledindinelli@users.noreply.github.com>
2019-11-23 11:22:33 +0100

    Popolate varie tabelle

--- Database/creazione_database.sql
+++ Database/creazione_database.sql
@@ -5,28 +5,28 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS Utente;
 
 CREATE TABLE Utente(
-	username      	VARCHAR(20) PRIMARY KEY,
+	username      		VARCHAR(20) PRIMARY KEY,
     nome          	VARCHAR(20) NOT NULL,
     cognome       	VARCHAR(20) NOT NULL,
     password      	VARCHAR(20) NOT NULL,
-	autorizzazione 	ENUM ('Utente','Admin') NOT NULL,
-	numero_carta 	VARCHAR(16),
-	intestatario	VARCHAR(40),
-	scadenza		DATE
+	autorizzazione 		ENUM ('Utente','Admin') NOT NULL,
+	numero_carta 			VARCHAR(16),
+	intestatario			VARCHAR(40),
+	scadenza					DATE
 );
 
 -- Inserimento dati nella tabella Utente
 
-INSERT INTO Utente (username, nome, cognome, password, autorizzazione, numero_carta, intestatario, scadenza) VALUES (
-('admin', 'Admin', 'Generico', 'password', 'Admin', NULL, NULL, NULL),
-('ammin1', 'Amministratore', 'Due', 'password', 'Admin', NULL, NULL, NULL),
-('ammin2', 'Amministratore', 'Tre', 'password', 'Admin', NULL, NULL, NULL),
-('utente', 'Utente', 'Generico', 'password', 'Utente', '1111222233334444', 'Utente Generico', ),
-('user1', 'Ulisse', 'Ferrari', 'password', 'Utente', '5555222211116666', 'Ulisse Ferrari', ),
-('user2', 'Jessica', 'Bianchi', 'password', 'Utente', '4444333388880000', 'Jessica Bianchi', ),
-('user3', 'Marco', 'Gasparotto', 'password', 'Utente', NULL, NULL, NULL),
-('user4', 'Betty', 'Basso', 'password', 'Utente', NULL, NULL, NULL),
-('user5', 'Stefano', 'Piana', 'password', 'Utente', '2222111100009999', 'Stefano Piana', )
+INSERT INTO Utente (username, nome_cognome, password, autorizzazione, numero_carta, intestatario, scadenza) VALUES (
+('admin', 'Admin Generico', 'password', 'Admin', NULL, NULL, NULL),
+('ammin1', 'Amministratore Due', 'password', 'Admin', NULL, NULL, NULL),
+('ammin2', 'Amministratore Tre', 'password', 'Admin', NULL, NULL, NULL),
+('utente', 'Utente Generico', 'password', 'Utente', '1111222233334444', 'Utente Generico', '2021-11-01'),
+('user1', 'User Uno', 'password', 'Utente', '5555222211116666', 'User Uno', '2024-01-01'),
+('user2', 'User Due', 'password', 'Utente', '4444333388880000', 'User Due', '2027-06-01'),
+('user3', 'User Tre', 'password', 'Utente', NULL, NULL, NULL),
+('user4', 'User Quattro', 'password', 'Utente', NULL, NULL, NULL),
+('user5', 'User Cinque', 'password', 'Utente', '2222111100009999', 'User Cinque', '2025-09-01')
 )
 
 -- Crea la tabella Recensione
@@ -35,11 +35,11 @@ DROP TABLE IF EXISTS Recensione;
 
 CREATE TABLE Recensione(
 	id_recensione 	INT PRIMARY KEY,
-	titolo		  	VARCHAR(30) NOT NULL,
-	testo			VARCHAR(150) NOT NULL,
-	data			DATE NOT NULL,
-	utente 		VARCHAR(20) NOT NULL,
-	FOREIGN KEY(utente) REFERENCES Utente(username) ON DELETE CASCADE
+	titolo		  		VARCHAR(30) NOT NULL,
+	testo						VARCHAR(150) NOT NULL,
+	data						DATE NOT NULL,
+	utente 					VARCHAR(20) NOT NULL,
+	FOREIGN KEY (utente) REFERENCES Utente(username) ON DELETE CASCADE
 );
 
 -- Inserimento dati nella tabella Recensione
@@ -48,31 +48,30 @@ INSERT INTO Recensione (id_recensione, titolo, testo, data, utente) VALUES (
 (001, 'Ristorante TOP', 'Sono un’appassionata di sushi e credo che questo ristorante possa vantare la migliore qualità e varietà della zona. Uramaki special strepitosi e servizio impeccabile.', '2018-09-11', 'user2'),
 (002, 'Bella serata', 'Ogni volta che vengo a Padova mi fermo sempre a cena in questo locale, i ragazzi dello staff sono simpatici e molto professionali, sulla qualità del cibo semplicemente ottimo.', '2019-03-22', 'user1'),
 (003, 'Consigliato', 'Bel ristorante in una zona molto accogliente di Padova. Non me ne intendo molto di sushi ma posso dire che lo consiglierò sicuramente ad amici', '2017-11-08', 'user3'),
-(004, 'Ho provato di meglio', 'Scoperto l’anno scorso, siamo tornati anche quest’anno. La qualità è sempre ottima sia dei crudi che nei cotti, con molta scelta. Peccato per i tavoli un po’ piccoli, quando arrivano più di due piatti diventa difficile gestire gli spazi. Prezzi e servizio nella media.', '2019-06-27', 'user4')
+(004, 'Ho provato di meglio', 'Scoperto l’anno scorso, siamo tornati anche quest’anno. La qualità è sempre ottima sia dei crudi che dei cotti, con molta scelta. Peccato per i tavoli un po’ piccoli, quando arrivano più di due piatti diventa difficile gestire gli spazi. Prezzi e servizio nella media.', '2019-06-27', 'user4')
 )
+
 -- Crea la tabella News
 
 DROP TABLE IF EXISTS News;
 
 CREATE TABLE News(
-	id_news		 	INT PRIMARY KEY,
+	id_news		 		INT PRIMARY KEY,
 	titolo		  	VARCHAR(30) NOT NULL,
 	descrizione		VARCHAR(150),
-	data			DATE NOT NULL,
-	utente 			VARCHAR(20) NOT NULL,
-	FOREIGN KEY(utente) REFERENCES Utente(username) ON DELETE CASCADE
+	data					DATE NOT NULL,
+	utente 				VARCHAR(20) NOT NULL,
+	FOREIGN KEY (utente) REFERENCES Utente(username) ON DELETE CASCADE
 );
 
 -- Inserimento dati nella tabella News
 
 INSERT INTO News (id_news, titolo, descrizione, data, utente) VALUES (
-(101, , , , 'admin'),
-(102, , , , 'ammin1'),
-(103, , , , 'ammin2'),
-(104, , , , 'admin'),
-(105, , , , 'ammin1'),
-(106, , , , 'ammin2'),
-(107, , , , 'admin'),
+(101, 'Ferragosto', 'Vi ricordiamo che dal 12 al 18 Agosto il ristorante rimarra chiuso per ferie.', '2019-08-08', 'ammin1'),
+(102, 'Cercasi Cameriera', 'Cercasi cameriera per contratto part-time ', '2019-08-22', 'ammin2'),
+(103, 'Black Friday Week', 'Dal 25 al 30 Novembre sconto del 10% su tutti gli ordini superiori ai 30€; Venerdì 29 sconto del 20%.', '2019-11-18', 'admin'),
+(104, 'Buon Natale', 'Lo staff di Sushi Nakamura augura a voi ed alle vostre famiglie un felice Natale.', '2019-12-24', 'ammin1'),
+(105, 'Felice Anno Nuovo', 'Per festeggiare il nuovo anno, sconto del 10% su tutti gli ordini superiori ai 20€ fino a Domenica 5.', '2020-01-02', 'ammin2')
 )
 
 -- Crea la tabella Destinazione
@@ -80,27 +79,26 @@ INSERT INTO News (id_news, titolo, descrizione, data, utente) VALUES (
 DROP TABLE IF EXISTS Destinazione;
 
 CREATE TABLE Destinazione(
-	id_destinazione		INT PRIMARY KEY,
-	nome				VARCHAR(20) NOT NULL,
-	cognome				VARCHAR(20) NOT NULL,
-	numero_telefonico	VARCHAR(15),
-	CAP					VARCHAR(5) NOT NULL,
-	via 				VARCHAR(15) NOT NULL,
-	numero_civico		SMALLINT NOT NULL,
-	utente 				VARCHAR(20) NOT NULL,
-	FOREIGN KEY(utente) REFERENCES Utente(username) ON DELETE CASCADE
+	id_destinazione			INT PRIMARY KEY,
+	nome_cognome				VARCHAR(40) NOT NULL,
+	numero_telefonico		VARCHAR(15),
+	CAP									VARCHAR(5) NOT NULL,
+	via 								VARCHAR(15) NOT NULL,
+	numero_civico				SMALLINT NOT NULL,
+	utente 							VARCHAR(20) NOT NULL,
+	FOREIGN KEY (utente) REFERENCES Utente(username) ON DELETE CASCADE
 );
 
 -- Inserimento dati nella tabella Destinazione
 
-INSERT INTO Destinazione (id_destinazione, nome, cognome, numero_telefonico, CAP, via, numero_civico, utente) VALUES (
-(201, 'Utente', 'Generico', '049XXXXXXX', '35100', 'Aldo Moro', 21, 'utente'),
-(202, 'Utente', 'Generico', '049XXXXXXX', '35100', 'Ugo Bassi', 17, 'utente'),
-(203, 'Ulisse', 'Ferrari', '346XXXXXXX', '35133', 'Don Stefani', 10, 'user1'),
-(204, 'Jessica', 'Bianchi', '339XXXXXXX', '35142', 'Monte Bianco', 15, 'user2'),
-(205, 'Marco', 'Gasparotto', '333XXXXXXX', '35129', 'Andrea Palladio', 17, 'user3'),
-(206, 'Betty', 'Basso', '340XXXXXXX', '35122', 'Antonio Canova', 11, 'user4'),
-(207, 'Stefano', 'Piana', '348XXXXXXX', '35100', 'Aldo Moro', 22, 'user5')
+INSERT INTO Destinazione (id_destinazione, nome_cognome, numero_telefonico, CAP, via, numero_civico, utente) VALUES (
+(201, 'Utente Generico', '049XXXXXXX', '35100', 'Aldo Moro', 21, 'utente'),
+(202, 'Utente Generico', '049XXXXXXX', '35100', 'Ugo Bassi', 17, 'utente'),
+(203, 'User Uno', '346XXXXXXX', '35133', 'Don Stefani', 10, 'user1'),
+(204, 'User Due', '339XXXXXXX', '35142', 'Monte Bianco', 15, 'user2'),
+(205, 'User Tre', '333XXXXXXX', '35129', 'Andrea Palladio', 17, 'user3'),
+(206, 'User Quattro', '340XXXXXXX', '35122', 'Antonio Canova', 11, 'user4'),
+(207, 'User Cinque', '348XXXXXXX', '35100', 'Aldo Moro', 22, 'user5')
 )
 
 -- Crea la tabella Tabella Ordine
@@ -108,28 +106,27 @@ INSERT INTO Destinazione (id_destinazione, nome, cognome, numero_telefonico, CAP
 DROP TABLE IF EXISTS Ordine;
 
 CREATE TABLE Ordine(
-	id_ordine		INT PRIMARY KEY,
+	id_ordine			INT PRIMARY KEY,
 	data_ordine		DATE NOT NULL,
-	data_consegna	DATE NOT NULL,
-	totale			FLOAT NOT NULL,
+	totale				FLOAT NOT NULL,
 	destinazione	INT NOT NULL,
-	FOREIGN KEY(destinazione) REFERENCES Destinazione(id_destinazione) ON DELETE NO ACTION
+	FOREIGN KEY (destinazione) REFERENCES Destinazione(id_destinazione) ON DELETE NO ACTION
 );
 
 -- Inserimento dati nella tabella Ordine
 
-INSERT INTO Ordine (id_ordine, data_ordine, data_consegna, totale, destinazione) VALUES (
-(301, , , 27.00, 201),
-(302, , , 28.00, 202),
-(303, , , 33.00, 203),
-(304, , , 31.50, 204),
-(305, , , 46.50, 205),
-(306, , , 40.00, 206),
-(307, , , 36.50, 207),
-(308, , , 66.00, 201),
-(309, , , 15.50, 202),
-(310, , , 42.50, 203),
-(311, , , 57.50, 204)
+INSERT INTO Ordine (id_ordine, data_ordine, totale, destinazione) VALUES (
+(301, '2019-02-19', 27.00, 201),
+(302, '2019-03-22', 28.00, 202),
+(303, '2019-04-11', 33.00, 203),
+(304, '2019-05-09', 31.50, 204),
+(305, '2019-05-07', 46.50, 205),
+(306, '2019-06-12', 40.00, 206),
+(307, '2019-07-22', 36.50, 207),
+(308, '2019-08-29', 66.00, 201),
+(309, '2019-10-16', 15.50, 202),
+(310, '2020-01-10', 42.50, 203),
+(311, '2020-01-13', 57.50, 204)
 )
 
 -- Crea la tabella Tabella Prodotto
@@ -137,9 +134,9 @@ INSERT INTO Ordine (id_ordine, data_ordine, data_consegna, totale, destinazione)
 DROP TABLE IF EXISTS Prodotto;
 
 CREATE TABLE Prodotto(
-	nome 			VARCHAR(30) PRIMARY KEY,
+	nome 				VARCHAR(30) PRIMARY KEY,
 	categoria		ENUM('Antipasti','Primi Piatti','Teppanyako e tempure','Uramaki','Nigiri','Gunkan','Temaki','Hosomaki','Sashimi','Dessert') NOT NULL,
-	pezzi			TINYINT NOT NULL,
+	pezzi				TINYINT NOT NULL,
 	prezzo 			FLOAT NOT NULL
 );
 
@@ -219,12 +216,12 @@ INSERT INTO Prodotto (nome, categoria, pezzi, prezzo) VALUES (
 DROP TABLE IF EXISTS Contiene;
 
 CREATE TABLE Contiene(
-    id_ordine 		INT,
-    nome 			VARCHAR(30),
+    id_ordine 			INT,
+    nome 						VARCHAR(30),
     numero_porzioni TINYINT NOT NULL,
-    PRIMARY KEY(id_ordine, nome),
-	FOREIGN KEY(id_ordine) REFERENCES Ordine(id_ordine) ON DELETE CASCADE,
-	FOREIGN KEY(nome) REFERENCES Prodotto(nome) ON DELETE NO ACTION
+    PRIMARY KEY (id_ordine, nome),
+	FOREIGN KEY (id_ordine) REFERENCES Ordine(id_ordine) ON DELETE CASCADE,
+	FOREIGN KEY (nome) REFERENCES Prodotto(nome) ON DELETE NO ACTION
 );
 
 -- Inserimento dati nella tabella Contiene
