8368971fd33b50e06a692064164b9e6cca6f436d
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-11 08:36:24 +0100

    Aggiunti alcuni commenti e fatta funzione per aprire o chiudere menu

--- Sito/js/index.js
+++ Sito/js/index.js
@@ -1,29 +1,33 @@
-var open = false;
-
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM completamente caricato e analizzato');
+    //console.log('DOM completamente caricato e analizzato');
 	
-	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	// funzione per salvare la larghezza dello schermo
+	var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 	
-	if( width <= 900 ) {
-		window.addEventListener("click",function(e1) {
-			if(open) {
-				document.getElementById("menu").classList.remove("open");
-				open = false;
-				console.log(open);
-				document.getElementsByTagName("body")[0].style.overflow = "auto";
-			}
+	if( deviceWidth <= 900 ) {
+		window.addEventListener("click",function(e) {
+			toggleMenu(e);
 		});
 
-		document.getElementById("menu").getElementsByTagName("ul")[0].addEventListener("click",function(e2) {
-			e2.stopPropagation();
-			if(!open) {
-				document.getElementById("menu").classList.add("open");
-				open = true;
-				console.log(open);
-				document.getElementsByTagName("body")[0].style.overflow = "hidden";
-			}
-			console.log("qui");
+		document.getElementById("menu").getElementsByTagName("ul")[0].addEventListener("click",function(e) {
+			e.stopPropagation();	// fa in modo che venga eseguito solo questo ascoltatore
+			toggleMenu(e);
 		});
 	}
 });
+
+var open = false;
+// funzione per aprire e chiudere il menu
+function toggleMenu() {
+	if(open) {
+		document.getElementById("menu").classList.remove("open");
+		document.getElementsByTagName("body")[0].style.overflow = "auto";
+		open = false;
+		//console.log("Menu aperto: "+open);
+	} else {
+		document.getElementById("menu").classList.add("open");
+		document.getElementsByTagName("body")[0].style.overflow = "hidden";
+		open = true;
+		//console.log("Menu aperto: "+open);
+	}
+}
