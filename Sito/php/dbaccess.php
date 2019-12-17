f19c7f8de082c1a0053c23772af8aab4ca2e9f91
Mirco <giardinamirco@gmail.com>
2019-12-17 20:35:21 +0100

    Creata funzione per il check delle credenziali

--- Sito/php/dbaccess.php
+++ Sito/php/dbaccess.php
@@ -1,59 +1,40 @@
-<? php
+<?php
 
-    class DBAccess 
+    class DBAccess
 	{
         const HOST_DB = 'localhost';
         const USERNAME = 'root';
         const PASSWORD = '';
         const DATABASE_NAME = 'Sushi'; //Ogni utente ha un database già creato con nome uguale alla propria login (scritto sulle slide)
+
+        public $connection = null;
+        public function openDBConnection()
+        {
+            $this->connection = mysqli_connect(static::HOST_DB,static::USERNAME, static::PASSWORD, static::DATABASE_NAME);
+            return $this->connection;
+        }
+
+        //Funzione per controllare le credenziali
+        public function checkLogin($username,$password)
+        {
+            $query = $this->connection->prepare('SELECT * FROM utente WHERE username= ? AND password= ?');
+            $query->bind_param('ss', $username,$password);
+            $query->execute();
+            $queryResult = $query->get_result();
+
+            /*while ($row = $queryResult->fetch_assoc()) {
+                echo $row['nome']." ".$row['cognome'];
+            }*/
+
+            if(mysqli_num_rows($queryResult) == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 
-    public $connection = null;
-    public function openDBConnection()
-	{
-        $this->connection = mysqli_connect(static::HOST_DB,static::USERNAME, static::PASSWORD, static::DATABASE_NAME);
-        if(!$this->connection)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
-	/*	Esempio di funzione per prendere i dati
-	public function getPersonaggi()
-	{
-		$query = "SELECT * FROM personaggi ORDER BY ID ASC";
-		$queryResult = myqsli_query($this->connection,$query);
-		
-		if(mysqli_num_rows($queryResult) == 0)
-		{
-			return null;
-		}
-		else
-		{
-			$result = array();
-			
-			while($row = mysqli_fetch_assoc($queryResult))
-			{
-				$arraySingoloPersonaggio = array(
-					'Nome' =>$row['nome]',
-					'Colore' => $row['colore'],
-					'Peso' => $row['peso'],
-					'Potenza' => $row['potenza'],
-					'Descrizione' => $row['descrizione'],
-					'ABR' => $row['angry_birds'],
-					'ABSW' => $row['angry_birds_star_wars'],
-					'AVS' => $row['angry_birds_space'],
-					'Immagine' => $row['immagine']
-				);
-				array_push($result,$arraySingoloPersonaggio);
-			}
-			
-			return $result;
-		}
-	}
-	*/
 ?>
