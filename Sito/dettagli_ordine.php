794cf8e2c0c99b449d84fe74682043bb26b4dfdf
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-20 08:28:52 +0100

    Fatta pagina dettagli_ordine con php

---
+++ Sito/dettagli_ordine.php
@@ -0,0 +1,56 @@
+<?php
+	require_once "php/dbaccess.php";
+	if( $_GET['id_ordine'] ) {
+		$db = new DBAccess();
+		
+		if( $db->openDBConnection() ) {
+			$username = 'user';	// da prendere a sessione
+			$dettagliOrdine = $db->getDettagliOrdine($_GET['id_ordine'],$username);
+			
+			if( is_object( $dettagliOrdine ) ) {
+				$content = '<h1>ID ordine: '.$dettagliOrdine->id_ordine.'</h1>
+					<h2 class="data">Data ordine: '.$dettagliOrdine->data_ordine.'</h2>
+					<h2 class="data">Data consegna: '.$dettagliOrdine->data_consegna.'</h2>
+
+					<p>Destinazione:<br/>'.
+					$dettagliOrdine->nome_cognome.'<br/>Via '.
+					$dettagliOrdine->via.' '.$dettagliOrdine->numero_civico.' CAP '.$dettagliOrdine->CAP.'<br/>Tel: '.$dettagliOrdine->numero_telefonico.
+					'<table class="defaultTable" summary="Lista prodotti ordinati">
+						<thead>
+							<tr>
+								<th scope="col">Nome prodotto</th>
+								<th scope="col">Categoria</th>
+								<th scope="col">Porzioni</th>
+							</tr>
+						</thead>
+						<tbody>';
+				
+				foreach( $dettagliOrdine->listaProdotti AS $row ) {
+					$content .=	'<tr>
+								<th scope="row" lang="ja">'.$row->nome.'</th>
+								<td><a href="prodotti.html#'.$row->categoria.'" lang="ja">'.$row->categoria.'</a></td>
+								<td>'.$row->numero_porzioni.'</td>
+							</tr>';
+				}
+				
+				$content .= '
+						</tbody>
+					</table>';
+				
+				$paginaHTML = file_get_contents('dettagli_ordine.html');
+				
+				echo str_replace('<infoOrdine/>',$content,$paginaHTML);
+			} else {
+				header("location: errore.html");
+				/* errore
+					l'id_ordine non esiste
+					l'username non ha effettuato l'ordine con quel id_ordine
+				*/
+			}
+		} else {
+			// errore connesione DB
+		}
+	} else {
+		header("location: errore.html");
+	}
+?>
--- Sito/php/dbaccess.php
+++ Sito/php/dbaccess.php
@@ -5,7 +5,7 @@
         const HOST_DB = 'localhost';
         const USERNAME = 'root';
         const PASSWORD = '';
-        const DATABASE_NAME = 'Sushi'; //Ogni utente ha un database già creato con nome uguale alla propria login (scritto sulle slide)
+        const DATABASE_NAME = 'progettosushi'; //Ogni utente ha un database già creato con nome uguale alla propria login (scritto sulle slide)
 
         public $connection = null;
         public function openDBConnection()
@@ -13,6 +13,38 @@
             $this->connection = mysqli_connect(static::HOST_DB,static::USERNAME, static::PASSWORD, static::DATABASE_NAME);
             return $this->connection;
         }
+		
+		public function getDettagliOrdine($id_ordine,$username) {
+			$query = $this->connection->prepare("SELECT O.*, D.* FROM Ordine O INNER JOIN Destinazione D ON O.destinazione = D.id_destinazione INNER JOIN Utente U ON D.utente = U.username WHERE U.username = ? AND O.id_ordine = ?");
+			$query->bind_param('ss',$username,$id_ordine);
+			$query->execute();
+			$queryResult = $query->get_result();
+			
+			if( $queryResult->num_rows > 0 ) {
+				$result = $queryResult->fetch_object();
+				
+				$query = $this->connection->prepare("SELECT C.*, P.categoria FROM Contiene C INNER JOIN Prodotto P ON C.nome = P.nome WHERE id_ordine = ?");
+				$query->bind_param('s',$id_ordine);
+				$query->execute();
+				$queryResult = $query->get_result();
+				
+				$listaProdotti = array();
+				
+				while ($row = $queryResult->fetch_object()) {
+					array_push($listaProdotti, $row);
+				}
+				
+				$result->listaProdotti = $listaProdotti;
+				
+				return $result;
+			} else {
+				/* errore
+					l'id_ordine non esiste
+					l'username non ha effettuato l'ordine con quel id_ordine
+				*/
+			}
+		}
+		
     }
 
 	/*	Esempio di funzione per prendere i dati
