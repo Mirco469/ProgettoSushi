f461499c248d28a0bc8635ae55e89ac443df148d
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-20 14:52:46 +0100

    Creata bozza per il carrello.php

---
+++ Sito/carrello.php
@@ -0,0 +1,48 @@
+<?php
+	require_once "php/dbaccess.php";
+		session_start();
+		$_SESSION['username'] = 'user';
+		$_SESSION['carrello'] = array(
+			0 => array(
+				'nome' => 'maguro',
+				'categoria' => 'uramaki',
+				'quantita' => 2,
+				'prezzo' => 4
+			)
+		);
+	
+	if( session_status() != PHP_SESSION_NONE && $_SESSION['username'] != null ) {
+		$db = new DBAccess();
+		
+		$carrello = $_SESSION['carrello'];
+		if( $_SESSION['carrello'] != null ) {
+			
+			$totale = 0;
+			$content = '<dl class="defaultLista">';
+			
+			foreach( $carrello AS $row ) {
+				$content .= '<dt>'.$row['nome'].' - <a href="prodotti.html#'.$row['categoria'].'">'.$row['categoria'].'</a> <input class="rimuovi" type="button" name="rimuovi" value="Rimuovi" /></dt>
+				<dd>
+					<input type="button" name="minus" value="-" /><!-- togli_prodotto -->
+					<input type="text" name="quantita" value="'.$row['quantita'].'" /><!-- numero prodotto -->
+					<input type="button" name="plus" value="+" /><!-- aggiungi_prodotto -->
+					<span>Prezzo: '.number_format($row['prezzo']*$row['quantita'], 2, ',', '.').'</span>
+				</dd>';
+				
+				$totale += $row['prezzo']*$row['quantita'];
+			}
+			
+			$content .= '</dl>
+			<p class="totaleText">Totale: <span>'.number_format($totale, 2, ',', '.').'€</span></p>
+			<a id="paga" href="pagamento.html">Vai a pagamento</a>';
+		
+		} else {
+			$content = '<p>Il carrello attualmente è vuoto. Se vuoi ordinare qualcosa recati alla pagina <a href="takeaway.php" lang="en">Take Away</a></p>';
+		}
+		$paginaHTML = file_get_contents('carrello.html');
+		
+		echo str_replace('<carrello/>',$content,$paginaHTML);
+	} else {
+		header("location: errore403.html");
+	}
+?>
