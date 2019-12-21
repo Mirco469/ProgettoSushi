b1a02d78487d9cc868efe325aabd6ab58ca20db3
Mirco <giardinamirco@gmail.com>
2019-12-21 18:14:58 +0100

    Fatta pagina di login dinamica
    close #423

---
+++ Sito/login.php
@@ -0,0 +1,147 @@
+<?php
+	require_once("php/dbaccess.php");
+
+    session_start();
+
+    //session_unset();
+    //session_destroy();
+
+    // Se si ha già una sessione attiva si viene reindirizzati verso la home
+    if(isset($_SESSION["username"]) && isset($_SESSION["autorizzazione"]))
+    {
+        //Mando alla home giusta in base all'autorizzazione
+        redirectHome($_SESSION["autorizzazione"]);
+    }
+    else
+    {
+        //Messaggi di errore rispettivamente per login e registrazione
+        $erroreL = "";
+        $erroreR = "";
+        //Campi per il form di login
+        $usernameL = "";
+        $passwordL = "";
+        //Campi per il form di registrazione
+        $usernameR = "";
+        $nomeR = "";
+        $cognomeR = "";
+        $passwordR = "";
+        $passwordRepeatR = "";
+
+        //Controllo se ho ricevuto la post dalla form di login o di registrazione o è la prima volta
+        if(isset($_POST['accedi']))
+        {
+            $usernameL = trim($_POST['username']);
+            $passwordL = trim($_POST['password']);
+
+            //Mi connetto al database
+            $oggettoConnessione =  new DBAccess();
+
+            if($oggettoConnessione->openDBConnection())
+            {
+                //Controllo se l'username e password ci sono del DB
+                $autorizzazione = $oggettoConnessione->checkLogin($usernameL,$passwordL);
+                if(isset($autorizzazione))
+                {
+                    $_SESSION["username"] = $usernameL;
+                    $_SESSION["autorizzazione"] = $autorizzazione;
+                    redirectHome($_SESSION["autorizzazione"]);
+                }
+                else
+                {
+                    $erroreL = "<p class='errore'>L'username o la password non sono corretti!</p>"; /*CLASSE errore DA CREARE*/
+                }
+            }
+            else
+            {
+                header("Location: /errore500.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+            }
+        }
+        elseif(isset($_POST['registrati']))
+        {
+            $usernameR = trim($_POST['username']);
+            $nomeR = trim($_POST['nome']);
+            $cognomeR = trim($_POST['cognome']);
+            $passwordR = trim($_POST['password']);
+            $passwordRepeatR = trim($_POST['passwordRepeat']);
+
+            //****************************************
+            //Controllare l'input e caricare sul database il nuovo account, infine reindirizzare nella home appropriata
+            //****************************************
+
+            // Controllo gli input
+            $oggettoConnessione =  new DBAccess();
+            if($oggettoConnessione->openDBConnection())
+            {
+                if($oggettoConnessione->alreadyExistsUsername($usernameR))
+                {
+                    $erroreR .= "<li>Username gi&agrave; utilizzato</li>";
+                }
+                if(!checkAlfanumerico($usernameR))
+                {
+                    $erroreR .= "<li>L'username deve contenere solo caratteri alfanumerici</li>";
+                }
+                if(!checkSoloLettereEDim($nomeR))
+                {
+                    $erroreR .= "<li>Il nome deve contenere solo lettere</li>";
+                }
+                if(!checkSoloLettereEDim($cognomeR))
+                {
+                    $erroreR .= "<li>Il cognome deve contenere solo lettere</li>";
+                }
+
+                if(!checkMinLen($passwordR))
+                {
+                    $erroreR .= "<li>La password deve essere lunga almeno due caratteri</li>";
+                }
+
+                if(strcmp($passwordR,$passwordRepeatR)!=0)
+                {
+                    $erroreR .= "<li>Le password non coincidono</li>";
+                }
+
+                //Controllo se non ho riscontrato errori
+                if($erroreR == "")
+                {
+                    $oggettoConnessione->addAccount($usernameR,$nomeR,$cognomeR,$passwordR);
+                }
+                else
+                {
+                    $erroreR = "<ul class='errore'>".$erroreR."</ul>";
+                }
+            }
+            else
+            {
+                header("Location: /errore500.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+            }
+        }
+
+        $paginaHTML = file_get_contents('html/login.html');
+
+        //Form di login
+        $formLoginContent = "
+                    <label for=\"nomeUtente\">Nome Utente:</label>
+                    <input type=\"text\" id=\"nomeUtente\" name=\"username\" placeholder=\"Utente\" required=\"required\" value=\"$usernameL\"/>
+                    <label for=\"passwordAcc\" lang=\"en\">Password:</label>
+                    <input type=\"password\" id=\"passwordAcc\" name=\"password\" placeholder=\"Password\" required=\"required\" value=\"$passwordL\"/>
+                    <input class=\"defaultButton\" type=\"submit\" name=\"accedi\" value=\"Accedi\"/>";
+
+        //Form di registrazione
+        $formRegistrazioneContent = "
+                    <label for=\"username\">Nome utente:</label>
+                    <input type=\"text\" id=\"username\" name=\"username\" placeholder=\"Utente\" required=\"required\" value=\"$usernameR\"/>
+                    <label for=\"nome\">Nome:</label>
+                    <input type=\"text\" id=\"nome\" name=\"nome\" placeholder=\"Nome\" required=\"required\" value=\"$nomeR\"/>
+                    <label for=\"cognome\">Cognome:</label>
+                    <input type=\"text\" id=\"cognome\" name=\"cognome\" placeholder=\"Cognome\" required=\"required\" value=\"$cognomeR\"/>
+                    <label for=\"passwordReg\" lang=\"en\">Password:</label>
+                    <input type=\"password\" id=\"passwordReg\" name=\"password\" required=\"required\" value=\"$passwordR\"/>
+                    <label for=\"passwordRepeat\">Ripeti la <span lang=\"en\">password:</span></label>
+                    <input type=\"password\" id=\"passwordRepeat\" name=\"passwordRepeat\" required=\"required\" value=\"$passwordRepeatR\"/>
+                    <input class=\"defaultButton\" type=\"submit\" name=\"registrati\" value=\"Registrati\"/>";
+
+        $paginaHTML = str_replace('<erroreLogin />', $erroreL, $paginaHTML);
+        $paginaHTML = str_replace('<formLogin />', $formLoginContent, $paginaHTML);
+        $paginaHTML = str_replace('<erroreRegistrazione />', $erroreR, $paginaHTML);
+        echo str_replace('<formRegistrazione />', $formRegistrazioneContent, $paginaHTML);
+    }
+?>
--- Sito/php/dbaccess.php
+++ Sito/php/dbaccess.php
@@ -14,7 +14,9 @@
             return $this->connection;
         }
 
-        //Funzione per controllare le credenziali
+        /* INIZIO FUNZIONI PAGINA LOGIN */
+
+        //Funzione per controllare le credenziali: ritorna null se non esiste alcuna corrispondenza altrimenti ritorna il suo livello di autorizzazione
         public function checkLogin($username,$password)
         {
             $query = $this->connection->prepare('SELECT * FROM utente WHERE username= ? AND password= ?');
@@ -22,10 +24,24 @@
             $query->execute();
             $queryResult = $query->get_result();
 
-            /*while ($row = $queryResult->fetch_assoc()) {
-                echo $row['nome']." ".$row['cognome'];
-            }*/
+            if(mysqli_num_rows($queryResult) == 0)
+            {
+                return null;
+            }
+            else
+            {
+                $row = $queryResult->fetch_assoc();
+                return $row['autorizzazione'];
+            }
+        }
 
+        //Funzione che controlla se l'username è già esistente: ritorna true se esiste già false altrimenti
+        public function  alreadyExistsUsername($username)
+        {
+            $query = $this->connection->prepare('SELECT * FROM utente WHERE username= ?');
+            $query->bind_param('s', $username);
+            $query->execute();
+            $queryResult = $query->get_result();
             if(mysqli_num_rows($queryResult) == 0)
             {
                 return false;
@@ -35,6 +51,73 @@
                 return true;
             }
         }
+
+        public function addAccount($username,$nome,$cognome,$password)
+        {
+            $query = $this->connection->prepare('INSERT INTO utente(username,nome,cognome,password,autorizzazione) VALUES (?,?,?,?,"Utente")');
+            $query->bind_param('ssss', $username,$nome,$cognome,$password);
+            if($query->execute())
+            {
+                redirectHome("Utente");
+            }
+            else
+            {
+                header("Location: /errore500.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+            }
+        }
+
+        /* FINE FUNZIONI PAGINA LOGIN*/
+
+    }
+
+    //Reindirizza alla home giusta in base all'autorizzazione passata come paramentro
+    function redirectHome($autorizzazione)
+    {
+        if(strcmp($autorizzazione,"Utente") == 0)
+        {
+            header("Location: home_utente.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+        }
+        elseif(strcmp($autorizzazione,"Admin") == 0)
+        {
+            header("Location: back/home_admin.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+        }
+        else
+        {
+            echo "Errore interno al server, contattare l'amministratore.";
+        }
+    }
+
+    //Controlla che la stringa sia lunga almeno due caratteri
+    function checkMinLen($string) {
+        if(strlen($string)<2){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    //Controlla che la stringa non contenga caratteri speciali
+    function checkAlfanumerico($string) {
+        if(!checkMinLen($string)) return false;
+        if (!preg_match('/^[a-zA-Z0-9]+$/', $string)) {
+            return false;
+        } else return true;
+    }
+
+    //Controlla che la stringa non contenga numeri e abbia almeno due caratteri
+    function checkSoloLettereEDim($string) {
+        if(!checkMinLen($string)) return false;
+        if (!preg_match('/^[a-zA-Z]+$/', $string)) {
+            return false;
+        } else return true;
+    }
+
+    //Controlla che la stringa contenga solo numeri e che sia lunga almeno due caratteri
+    function checkSoloNumerieDim($string){
+        if(!checkMinLen($string)) return false;
+        if (!preg_match('/[^0-9]+$/', $string)) {
+            return false;
+        } else return true;
     }
 
 ?>
