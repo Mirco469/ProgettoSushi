27639764b6cd001dc62a5c1488d44e58ef064409
Alessandro <tommasinalessandro@gmail.com>
2019-12-24 12:15:43 +0100

    Cambiata la funzione che ritorna menu
    Ora la funzione si chiama getMenu e ritorna la stringa con la parte giusta di menu

--- Sito/php/dbaccess.php
+++ Sito/php/dbaccess.php
@@ -228,13 +228,11 @@
         } else return true;
     }
 
-	//Stampa il menu a seconda che l'utente sia autenticato o meno
-	//Va passato il contenuto della pagina come parametro
-	function printMenu($paginaHTML) {
-	$menu = '';
-		if(isset($_SESSION['username'])) {
+		//Ritorna la parte di menu corretta a seconda che l'utente sia loggato o meno
+	function getMenu() {
 
-			$menu = '<li class="impostazioni">
+		if(isset($_SESSION['username'])) {
+		    return '<li class="impostazioni">
 						<span id="dropbtn">Area Riservata</span>
 						<ul id="dropdown_content">
 							<li><a href="carrello.html" tabindex="8">Carrello</a></li>
@@ -243,10 +241,9 @@
 							<li><a lang="en" href="#" tabindex="11">Logout</a></li>
 						</ul>
 					</li>';
-			return str_replace('<menu />', $menu, $paginaHTML);
+
 		}else {
-			$menu = '<li class="login"><a href="login.html" tabindex="7"><span lang="en">Login</span>/Registrazione</a></li>';
-		   return str_replace('<menu />', $menu, $paginaHTML);
+			return '<li class="login"><a href="login.html" tabindex="7"><span lang="en">Login</span>/Registrazione</a></li>';
 		}
 	}
 
