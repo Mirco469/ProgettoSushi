8681f04fcc956c5c264485ad4398224948b89912
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-30 14:55:15 +0100

    Aggiunti menù dinamico e errori 500 e 403 da controllare

--- Sito/storico_ordini.php
+++ Sito/storico_ordini.php
@@ -2,23 +2,39 @@
 	require_once('php/dbaccess.php');
 	$db = new DBAccess();
 	$connected = $db->openDBConnection();
-	
+	session_start();
 	if( $connected ) {
-		$ordini = $db->getOrdini('utente');
-		
-		$listaOrdini = '<dl class="defaultLista">';
-		
-		foreach( $ordini AS $row ) {
-			$listaOrdini .= '<dt class="idordine">ID ordine: '.$row->id_ordine.' <a class="buttonSmall" href="dettagli_ordine.php?id_ordine='.$row->id_ordine.'">Dettagli Ordine</a></dt>
-			<dd>
-				<span>Data ordine: '.$row->data_ordine.'</span>
-				<span>Data consegna: '.$row->data_consegna.'</span>
-				<span>Totale: '.number_format($row->totale,2,',','.').'€</span>
-			</dd>';
+		if( isset($_SESSION['username']) ) {
+			$paginaHTML = file_get_contents('html/home_utente.html');
+			$menu = getMenu();
+			$ordini = $db->getOrdini($_SESSION['username']);
+			
+			if( count($ordini) > 0 ) {
+				$listaOrdini = '<dl class="defaultLista">';
+				
+				foreach( $ordini AS $row ) {
+					$listaOrdini .= '<dt class="idordine">ID ordine: '.$row->id_ordine.' <a class="buttonSmall" href="dettagli_ordine.php?id_ordine='.$row->id_ordine.'">Dettagli Ordine</a></dt>
+					<dd>
+						<span>Data ordine: '.$row->data_ordine.'</span>
+						<span>Data consegna: '.$row->data_consegna.'</span>
+						<span>Totale: '.number_format($row->totale,2,',','.').'€</span>
+					</dd>';
+				}
+				$listaOrdini .= '</dl>';
+			} else {
+				$listaOrdini = '<h2>La lista è vuota.</h2>';
+			}
+			
+			$paginaHTML = file_get_contents('html/storico_ordini.html');
+			
+			$paginaHTML = str_replace('<menu />', $menu, $paginaHTML);
+			$paginaHTML = str_replace('<listaOrdini/>',$listaOrdini,$paginaHTML);
+			
+			echo $paginaHTML;
+		} else {
+			header("Location: errore403.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
 		}
-		$listaOrdini .= '</dl>';
-		
-		$paginaHTML = file_get_contents('storico_ordini.html');
-		echo str_replace('<listaOrdini/>',$listaOrdini,$paginaHTML);
+	} else {
+		header("Location: errore500.php"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
 	}
 ?>
