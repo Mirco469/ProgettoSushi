c086b8041bdd8b5ec171477dda920f7314ee55f0
Nicolò <nicolo.frison.1@studenti.unipd.it>
2020-01-03 08:48:53 +0100

    Spostato dettagli_ordine html in cartella html e modifiche al php

--- Sito/dettagli_ordine.php
+++ Sito/dettagli_ordine.php
@@ -1,57 +1,63 @@
 <?php
 	require_once "php/dbaccess.php";
-	if( $_GET['id_ordine'] ) {
-		$db = new DBAccess();
-		
-		if( $db->openDBConnection() ) {
-			$username = 'user';	// da prendere a sessione
-			$dettagliOrdine = $db->getDettagliOrdine($_GET['id_ordine'],$username);
+	session_start();
+	
+	if( $_SESSION['username'] ) {
+		if( $_GET['id_ordine'] ) {
+			$db = new DBAccess();
 			
-			if( is_object( $dettagliOrdine ) ) {
-				$content = '<h1>ID ordine: '.$dettagliOrdine->id_ordine.'</h1>
-					<h2 class="data">Data ordine: '.$dettagliOrdine->data_ordine.'</h2>
-					<h2 class="data">Data consegna: '.$dettagliOrdine->data_consegna.'</h2>
+			if( $db->openDBConnection() ) {
+				$username = 'user';	// da prendere a sessione
+				$dettagliOrdine = $db->getDettagliOrdine($_GET['id_ordine'],$username);
+				
+				if( is_object( $dettagliOrdine ) ) {
+					$content = '<h1>ID ordine: '.$dettagliOrdine->id_ordine.'</h1>
+						<h2 class="data">Data ordine: '.$dettagliOrdine->data_ordine.'</h2>
+						<h2 class="data">Data consegna: '.$dettagliOrdine->data_consegna.'</h2>
 
-					<p>Destinazione:<br/>'.
-					$dettagliOrdine->nome_cognome.'<br/>Via '.
-					$dettagliOrdine->via.' '.$dettagliOrdine->numero_civico.' CAP '.$dettagliOrdine->CAP.'<br/>Tel: '.$dettagliOrdine->numero_telefonico.
-					'<table class="defaultTable" summary="Lista prodotti ordinati">
-						<thead>
-							<tr>
-								<th scope="col">Nome prodotto</th>
-								<th scope="col">Categoria</th>
-								<th scope="col">Porzioni</th>
-							</tr>
-						</thead>
-						<tbody>';
-				
-				foreach( $dettagliOrdine->listaProdotti AS $row ) {
-					$content .=	'<tr>
-								<th scope="row" lang="ja">'.$row->nome.'</th>
-								<td><a href="prodotti.html#'.$row->categoria.'" lang="ja">'.$row->categoria.'</a></td>
-								<td>'.$row->numero_porzioni.'</td>
-							</tr>';
+						<p>Destinazione:<br/>'.
+						$dettagliOrdine->nome_cognome.'<br/>Via '.
+						$dettagliOrdine->via.' '.$dettagliOrdine->numero_civico.' CAP '.$dettagliOrdine->CAP.'<br/>Tel: '.$dettagliOrdine->numero_telefonico.
+						'<table class="defaultTable" summary="Lista prodotti ordinati">
+							<thead>
+								<tr>
+									<th scope="col">Nome prodotto</th>
+									<th scope="col">Categoria</th>
+									<th scope="col">Porzioni</th>
+								</tr>
+							</thead>
+							<tbody>';
+					
+					foreach( $dettagliOrdine->listaProdotti AS $row ) {
+						$content .=	'<tr>
+									<th scope="row" lang="ja">'.$row->nome.'</th>
+									<td><a href="prodotti.html#'.$row->categoria.'" lang="ja">'.$row->categoria.'</a></td>
+									<td>'.$row->numero_porzioni.'</td>
+								</tr>';
+					}
+					
+					$content .= '
+							</tbody>
+						</table>';
+					
+					$paginaHTML = file_get_contents('html/dettagli_ordine.html');
+					
+					echo str_replace('<id_ordine/>', $dettagliOrdine->id_ordine, str_replace('<infoOrdine/>',$content,$paginaHTML) );
+				} else {
+					/* errore
+						l'id_ordine non esiste
+						l'username non ha effettuato l'ordine con quel id_ordine
+					*/
+					header('location: errore403.php');
 				}
-				
-				$content .= '
-						</tbody>
-					</table>';
-				
-				$paginaHTML = file_get_contents('dettagli_ordine.html');
-				
-				echo str_replace('<id_ordine/>', $dettagliOrdine->id_ordine, str_replace('<infoOrdine/>',$content,$paginaHTML) );
 			} else {
-				/* errore
-					l'id_ordine non esiste
-					l'username non ha effettuato l'ordine con quel id_ordine
-				*/
-				header('location: errore403.html');
+				// errore connesione DB
+				header('location: errore500.php');
 			}
 		} else {
-			// errore connesione DB
-			header('location: errore500.html');
+			header("location: errore404.php");
 		}
 	} else {
-		header("location: errore404.html");
+		header('location: errore403.php');
 	}
 ?>
