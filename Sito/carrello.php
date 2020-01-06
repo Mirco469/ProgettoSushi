d291b74f8164fc4a313526a84cc2d97a95cac2ad
Nicolò <nicolo.frison.1@studenti.unipd.it>
2020-01-06 10:54:02 +0100

    Aggiunte funzioni js e php ajax in carrello

--- Sito/carrello.php
+++ Sito/carrello.php
@@ -1,51 +1,121 @@
 <?php
 	require_once "php/dbaccess.php";
-		session_start();
-		$_SESSION['username'] = 'user';
+	
+	session_start();
+	
+	if( isset($_POST['action']) ) {
+		if( $_POST['action'] == 'edit' ) {
+			changeQuantity($_POST['name'],$_POST['category'],intval($_POST['quantity']));
+		} else if($_POST['action'] == 'remove') {
+			removeProduct($_POST['name'],$_POST['category']);
+		}
+		//echo json_encode(array('index'=>$_POST['index'],'amount'=>$_POST['amount']));exit;
+	} else {
 		$_SESSION['carrello'] = array(
 			0 => array(
 				'nome' => 'maguro',
 				'categoria' => 'uramaki',
 				'quantita' => 2,
 				'prezzo' => 4
+			),
+			1 => array(
+				'nome' => 'tartara di tonno',
+				'categoria' => 'antipasti',
+				'quantita' => 1,
+				'prezzo' => 6
 			)
 		);
-	
-	if( session_status() != PHP_SESSION_NONE && $_SESSION['username'] != null ) {
-		$db = new DBAccess();
+		loadPage();
+	}
 		
-		$carrello = $_SESSION['carrello'];
-		if( $_SESSION['carrello'] != null ) {
+	function loadPage() {
+		if( session_status() != PHP_SESSION_NONE && $_SESSION['username'] != null ) {
+			$db = new DBAccess();
 			
-			$totale = 0;
-			$content = '<dl class="defaultLista">';
-			
-			foreach( $carrello AS $row ) {
-				$content .= '<dt>'.$row['nome'].' - <a href="prodotti.html#'.$row['categoria'].'">'.$row['categoria'].'</a> <input class="rimuovi" type="button" name="rimuovi" value="Rimuovi" /></dt>
-				<dd>
-					<input type="button" name="minus" value="-" /><!-- togli_prodotto -->
-					<input type="text" name="quantita" value="'.$row['quantita'].'" /><!-- numero prodotto -->
-					<input type="button" name="plus" value="+" /><!-- aggiungi_prodotto -->
-					<span>Prezzo: '.number_format($row['prezzo']*$row['quantita'], 2, ',', '.').'</span>
-				</dd>';
+			$carrello = $_SESSION['carrello'];
+			if( $_SESSION['carrello'] != null ) {
 				
-				$totale += $row['prezzo']*$row['quantita'];
-			}
+				$totale = 0;
+				$content = '<dl class="defaultLista">';
+				
+				foreach( $carrello AS $row ) {
+					$content .= '<dt>'.$row['nome'].' - <a href="prodotti.html#'.$row['categoria'].'">'.$row['categoria'].'</a><input class="rimuovi" type="button" name="rimuovi" value="Rimuovi" /></dt>
+					<dd>
+						<input type="button" name="minus" value="-" /><!-- togli_prodotto -->
+						<input type="number" name="quantita" readonly="readonly" value="'.$row['quantita'].'" /><!-- numero prodotto -->
+						<input type="button" name="plus" value="+" /><!-- aggiungi_prodotto -->
+						<span>Prezzo: '.number_format($row['prezzo'], 2, ',', '.').'€</span>
+					</dd>';
+				}
+				
+				$content .= '</dl>
+				<p class="totaleText">Totale: <span>'.number_format(getCartTotal(), 2, ',', '.').'€</span></p>
+				<a id="paga" href="pagamento.html">Vai a pagamento</a>';
 			
-			$content .= '</dl>
-			<p class="totaleText">Totale: <span>'.number_format($totale, 2, ',', '.').'€</span></p>
-			<a id="paga" href="pagamento.html">Vai a pagamento</a>';
+			} else {
+				$content = '<p>Il carrello attualmente è vuoto. Se vuoi ordinare qualcosa recati alla pagina <a href="takeaway.php" lang="en">Take Away</a></p>';
+			}
+			$paginaHTML = file_get_contents('html/carrello.html');
 		
+			$paginaHTML = str_replace('<menu />',getMenu(),$paginaHTML);
+			$paginaHTML = str_replace('<carrello/>',$content,$paginaHTML);
+			
+			echo $paginaHTML;
 		} else {
-			$content = '<p>Il carrello attualmente è vuoto. Se vuoi ordinare qualcosa recati alla pagina <a href="takeaway.php" lang="en">Take Away</a></p>';
+			header("location: errore403.html");
 		}
-		$paginaHTML = file_get_contents('html/carrello.html');
+	}
+	
+	function changeQuantity($name,$category,$newQuantity) {
+		$i = getIndexByNameCategory($name,$category);
 		
-		$paginaHTML = str_replace('<menu />',getMenu(),$paginaHTML);
-		$paginaHTML = str_replace('<carrello/>',$content,$paginaHTML);
+		$oldQuantity = $_SESSION['carrello'][$i]['quantita'];
+		$newPrezzo = $_SESSION['carrello'][$i]['prezzo']/$oldQuantity*$newQuantity;
 		
-		echo $paginaHTML;
-	} else {
-		header("location: errore403.html");
+		$_SESSION['carrello'][$i]['quantita'] = $newQuantity;
+		$_SESSION['carrello'][$i]['prezzo'] = $newPrezzo;
+		
+		header('Content-Type: application/json');
+		echo json_encode(array(
+			'success'=>true,
+			'price'=>$newPrezzo,
+			'quantity'=>$newQuantity,
+			'total'=>getCartTotal(),
+			'carrello'=>$_SESSION['carrello'],
+			'index'=>$i
+		));
+	}
+	
+	function removeProduct($name,$category) {
+		$i = getIndexByNameCategory($name,$category);
+		
+		unset($_SESSION['carrello'][$i]);
+		
+		header('Content-Type: application/json');
+		echo json_encode(array(
+			'success'=>true,
+			'total'=>getCartTotal()
+		));
+	}
+	
+	function getIndexByNameCategory($name,$category) {
+		foreach( $_SESSION['carrello'] AS $index => $row ) {
+			if( $row['nome'] === $name ) {
+				if( $row['categoria'] === $category ) {
+					return $index;
+				}
+			}
+		}
+		return -1;
+	}
+
+	function getCartTotal() {
+		$totale = 0;
+		
+		foreach( $_SESSION['carrello'] AS $row ) {
+			$totale += $row['prezzo'];
+		}
+		
+		return $totale;
 	}
 ?>
---
+++ Sito/js/carrello.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	document.getElementsByName('minus').forEach(function(item, index) {
+		item.addEventListener("click",function(e) {
+			var quantita = parseInt(document.getElementsByName('quantita')[index].value)-1;
+			
+			var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
+			changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
+		});
+	}); 
+	
+	document.getElementsByName('plus').forEach(function(item, index) {
+		item.addEventListener("click",function(e) {
+			var quantita = parseInt(document.getElementsByName('quantita')[index].value)+1;
+			
+			var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
+			changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
+		});
+	}); 
+	
+	document.getElementsByName('rimuovi').forEach(function(item, index) {
+		item.addEventListener("click",function(e) {
+			var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
+			removeProduct(index, dt[0].trim(), dt[1].trim());
+		});
+	}); 
+});
+
+function changeQuantity(index, name, category, quantity) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){ //quando l’operazione è completata
+		if(xhr.readyState ==4){
+			var data = JSON.parse(xhr.responseText);
+			console.log(data);
+			
+			document.getElementsByName('quantita')[index].value = data.quantity;
+			document.getElementsByTagName('dd')[index].getElementsByTagName('span')[0].textContent = 'Prezzo: '+parseFloat(data.price).toFixed(2).replace('.',',')+'€';
+			document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+		}	
+	}
+	xhr.open("POST", "carrello.php", true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("action=edit&name="+name+"&category="+category+"&quantity="+quantity);
+}
+
+function removeProduct(index, name, category) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){ //quando l’operazione è completata
+		if(xhr.readyState ==4){
+			var data = JSON.parse(xhr.responseText);
+			console.log(data);
+			
+			if(data.success) {
+				document.getElementsByTagName('dt')[index].remove();
+				document.getElementsByTagName('dd')[index].remove();
+				
+				document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+			}
+		}	
+	}
+	xhr.open("POST", "carrello.php", true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("action=remove&name="+name+"&category="+category);
+}
+
