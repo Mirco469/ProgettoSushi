5b67cc3eff14e61ea6a6ca2710310fd3e96cbd16
Nicolò <nicolo.frison.1@studenti.unipd.it>
2020-01-06 18:28:13 +0100

    Update carrello.js

--- Sito/js/carrello.js
+++ Sito/js/carrello.js
@@ -1,20 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	document.getElementsByName('minus').forEach(function(item, index) {
-		item.addEventListener("click",function(e) {
+		item.onclick = function(e) {
 			var quantita = parseInt(document.getElementsByName('quantita')[index].value)-1;
 			
-			var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
-			changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
-		});
+			if(quantita>=1) {
+				var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
+				changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
+			}
+		};
 	}); 
 	
-	document.getElementsByName('plus').forEach(function(item, index) {
-		item.addEventListener("click",function(e) {
+	document.getElementsByName('plus').forEach(function(item, index) {;
+		item.onclick = function(e) {
 			var quantita = parseInt(document.getElementsByName('quantita')[index].value)+1;
 			
-			var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
-			changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
-		});
+			if(quantita<=100) {	// limito la quantita massima di un prodotto a 100 porzioni
+				var dt = document.getElementsByTagName('dt')[index].textContent.split(' - ');
+				changeQuantity(index, dt[0].trim(), dt[1].trim(), quantita);
+			}
+		};
 	}); 
 	
 	document.getElementsByName('rimuovi').forEach(function(item, index) {
@@ -32,9 +36,13 @@ function changeQuantity(index, name, category, quantity) {
 			var data = JSON.parse(xhr.responseText);
 			console.log(data);
 			
-			document.getElementsByName('quantita')[index].value = data.quantity;
-			document.getElementsByTagName('dd')[index].getElementsByTagName('span')[0].textContent = 'Prezzo: '+parseFloat(data.price).toFixed(2).replace('.',',')+'€';
-			document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+			if( data.success ) {
+				document.getElementsByName('quantita')[index].value = data.quantity;
+				document.getElementsByTagName('dd')[index].getElementsByTagName('span')[0].textContent = 'Prezzo: '+parseFloat(data.price).toFixed(2).replace('.',',')+'€';
+				document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+			} else {
+				// errore modifica quantita
+			}
 		}	
 	}
 	xhr.open("POST", "carrello.php", true);
@@ -50,10 +58,14 @@ function removeProduct(index, name, category) {
 			console.log(data);
 			
 			if(data.success) {
-				document.getElementsByTagName('dt')[index].remove();
-				document.getElementsByTagName('dd')[index].remove();
-				
-				document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+				if( document.getElementsByTagName('dt').length > 1 ) {
+					document.getElementsByTagName('dt')[index].remove();
+					document.getElementsByTagName('dd')[index].remove();
+					
+					document.getElementsByClassName('totaleText')[0].getElementsByTagName('span')[0].textContent = parseFloat(data.total).toFixed(2).replace('.',',')+'€';
+				} else {
+					window.location.reload();
+				}
 			}
 		}	
 	}
