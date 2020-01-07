1f7951f10bd3f0305b75a9e6293d1c179e31e8f5
Alessandro <alessandro.dindinelli@gmail.com>
2020-01-07 16:42:03 +0100

    Tolti commenti per Sonarcloud

--- Sito/recensioni.php
+++ Sito/recensioni.php
@@ -12,21 +12,18 @@
         $menu = getmenu();
 	    $paginaHTML = str_replace('<menu />', $menu, $paginaHTML);
         
-        #Inizializzo il messaggio ed i campi del form;
         $form = "";
         $messaggio = "";
         $titolo = "";
         $testo = "";
-        #Se l'utente è loggato mostro il form per scrivere una recensione;
+
         if (isset($_SESSION["username"]))
         {
-            //Controllo se ho ricevuto la post dalla form o se è la prima volta;
             if(isset($_POST['invia']))
             {
                 $titolo = htmlentities(trim($_POST['titolo']));
                 $testo = htmlentities(stripslashes(trim($_POST['testo'])));
 
-                #Controllo gli input;
                 if (!checkTestoSpaziDim($titolo, 6))
                 {
                     $messaggio .= "<li>Il titolo deve contenere solo lettere e spaziature interne ed essere almeno lungo 6 caratteri</li>";
@@ -38,7 +35,7 @@
 
                 if ($messaggio == "")
                 {
-                    $data = getdate(); #L'array data ha anche i campi [year][mon][mday];
+                    $data = getdate();
                     $data = "$data[year]-$data[mon]-$data[mday]";
                     $oggettoConnessione->addRecensione($titolo, $data, $_SESSION["username"], $testo);
                     $messaggio = "<p class='successo'>Recensione aggiunta con successo!</p>";
@@ -68,7 +65,6 @@
         $paginaHTML = str_replace('<formRecensione />', $form, $paginaHTML);
         $paginaHTML = str_replace('<messaggio />', $messaggio, $paginaHTML);
 
-        #Stampo le recensioni esistenti nel database;
         $listaRecensioni = "
     	    <div id=\"lista_recensioni\">
     		<dl>";
@@ -91,7 +87,7 @@
         $paginaHTML = str_replace('<listaRecensioni />', $listaRecensioni, $paginaHTML);
         echo $paginaHTML;
     }
-    else #Errore di connessione al databse;
+    else
 	{
 		header("Location: /errore500.php");
 	}
