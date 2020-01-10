a5798222c9460906db0c789cd23309280941d304
Mirco <giardinamirco@gmail.com>
2020-01-10 11:28:59 +0100

    Corretto input utente che si resettavano

--- Sito/back/modifica_prodotto.php
+++ Sito/back/modifica_prodotto.php
@@ -9,18 +9,33 @@
         //Se non ho l'autorizzazione di admin mando alla pagina 403
         if(strcmp($_SESSION["autorizzazione"],"Admin") == 0)
         {
-
-            $erroreModifica = "";
+			$erroreModifica = "";
             //Campi per il form di aggiunta
             $nomeProdotto = "";
             $categoria = "";
             $numeroPezzi = "";
             $prezzo = "";
             $descrizione = "";
+			
 
             $oggettoConnessione =  new DBAccess();
             if($oggettoConnessione->openDBConnection())
             {
+				$prodotto = $oggettoConnessione->getInfoProdotto($_GET['nome']);
+				if(isset($prodotto))
+				{
+					$nomeProdotto = $_GET['nome'];
+					$categoria = $prodotto["categoria"];
+					$numeroPezzi = $prodotto["pezzi"];
+					$prezzo = $prodotto["prezzo"];
+					$descrizione = $prodotto["descrizione"];
+				}
+				else
+				{
+					header("Location: errore500.html"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
+					exit;
+				}
+				
                 if(isset($_POST['annulla']))
                 {
                     header('location: aggiunta_prodotti.php');
@@ -75,46 +90,37 @@
                     }
                 }
 
+				$option = '<option value="Antipasti">Antipasti</option>
+						<option value="Primi Piatti">Primi Piatti</option>
+						<option value="Teppanyako e Tempure" lang="ja">Teppanyako e tempure</option>
+						<option value="Uramaki" lang="ja">Uramaki</option>
+						<option value="Nigiri ed Onigiri" lang="ja">Nigiri ed Onigiri</option>
+						<option value="Gunkan" lang="ja">Gunkan</option>
+						<option value="Temaki" lang="ja">Temaki</option>
+						<option value="Hosomaki" lang="ja">Hosomaki</option>
+						<option value="Sashimi" lang="ja">Sashimi</option>
+						<option value="Dessert" lang="fr">Dessert</option>';
+				$option = preg_replace('/'.$categoria.'"/', $categoria.'" selected="selected"', $option, 1);
 
-                $prodotto = $oggettoConnessione->getInfoProdotto($_GET['nome']);
-                if(isset($prodotto))
-                {
-                    $option = '<option value="Antipasti">Antipasti</option>
-                            <option value="Primi Piatti">Primi Piatti</option>
-                            <option value="Teppanyako e Tempure" lang="ja">Teppanyako e tempure</option>
-                            <option value="Uramaki" lang="ja">Uramaki</option>
-                            <option value="Nigiri ed Onigiri" lang="ja">Nigiri ed Onigiri</option>
-                            <option value="Gunkan" lang="ja">Gunkan</option>
-                            <option value="Temaki" lang="ja">Temaki</option>
-                            <option value="Hosomaki" lang="ja">Hosomaki</option>
-                            <option value="Sashimi" lang="ja">Sashimi</option>
-                            <option value="Dessert" lang="fr">Dessert</option>';
-                    $option = preg_replace('/'.$prodotto["categoria"].'"/', $prodotto["categoria"].'" selected="selected"', $option, 1);
-
-                    $formModifica = "
-                        <label for=\"prodotto\">Nome Prodotto:</label>
-					    <input type=\"text\" id=\"prodotto\" name=\"prodotto\" value=\"".$_GET['nome']."\" readonly=\"readonly\"/>
-					    <label for=\"categoria\">Categoria:</label>
-                        <select id=\"categoria\" name=\"categoria\">
-                            ".$option."
-                        </select>
-                        <label for=\"porzione\">Numero pezzi:</label>
-                        <input type=\"text\" id=\"porzione\" name=\"porzione\" value=\"".$prodotto["pezzi"]."\"/>
-                        <label for=\"prezzo\">Prezzo:</label>
-                        <input type=\"text\" id=\"prezzo\" name=\"prezzo\" value=\"".$prodotto["prezzo"]."\"/>
-                        <label for=\"descrizione\">Descrizione:</label>
-                        <textarea id=\"descrizione\" name=\"descrizione\" rows=\"4\" cols=\"35\">".$prodotto["descrizione"]."</textarea>";
+				$formModifica = "
+					<label for=\"prodotto\">Nome Prodotto:</label>
+					<input type=\"text\" id=\"prodotto\" name=\"prodotto\" value=\"".$nomeProdotto."\" readonly=\"readonly\"/>
+					<label for=\"categoria\">Categoria:</label>
+					<select id=\"categoria\" name=\"categoria\">
+						".$option."
+					</select>
+					<label for=\"porzione\">Numero pezzi:</label>
+					<input type=\"text\" id=\"porzione\" name=\"porzione\" value=\"".$numeroPezzi."\"/>
+					<label for=\"prezzo\">Prezzo:</label>
+					<input type=\"text\" id=\"prezzo\" name=\"prezzo\" value=\"".$prezzo."\"/>
+					<label for=\"descrizione\">Descrizione:</label>
+					<textarea id=\"descrizione\" name=\"descrizione\" rows=\"4\" cols=\"35\">".$descrizione."</textarea>";
 
 
-                    $paginaHTML = file_get_contents('html/modifica_prodotto.html');
-                    $paginaHTML = str_replace('<erroreModifica />', $erroreModifica, $paginaHTML);
-                    $paginaHTML = str_replace('<formModifica />', $formModifica, $paginaHTML);
+				$paginaHTML = file_get_contents('html/modifica_prodotto.html');
+				$paginaHTML = str_replace('<erroreModifica />', $erroreModifica, $paginaHTML);
+				$paginaHTML = str_replace('<formModifica />', $formModifica, $paginaHTML);
                     echo $paginaHTML;
-                }
-                else
-                {
-                    header("Location: errore500.html"); /*CONTROLLARE SE LA PAGINA E' GIUSTA*/
-                }
             }
             else
             {
