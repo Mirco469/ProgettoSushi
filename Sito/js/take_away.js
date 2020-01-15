c951257ceaa6bcf7807a65a5286413c48eafcbb9
Alessandro <alessandro.dindinelli@gmail.com>
2020-01-15 22:09:46 +0100

    Test messaggio d'errore

--- Sito/js/take_away.js
+++ Sito/js/take_away.js
@@ -2,27 +2,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	document.getElementsByName('Aggiungi').forEach(function(item,index) { // cerco gli elementi per input perchè so che gli unici input presenti sono per l'aggiungi
 		item.onclick = function(e) {
 			var name = item.parentNode.textContent; // prendo il nome del prodotto
-			aggiungi(name);
-
-			// va bene metterlo qua o dentro aggiungi???
 			var dt = item.parentNode; // seleziono il dt
-			var dd = document.createElement("dd"); // creo il dd
-			dd.textContent = "Prodotto aggiunto al carrello!"; // scrivo il testo del messaggio
-			dd.classList.add("successo"); // assegno la classe al dd
-			dt.parentNode.insertBefore(dd, dt.nextSibling); // inserisco il dd
+			aggiungi(name, dt);
 		};
 	});
 });
 
-function aggiungi(name) {
+function aggiungi(name, dt) {
 	var xhr = new XMLHttpRequest();	//istanziazione dell'oggetto richiesta
 	xhr.onreadystatechange = function() { //funzione richiamata quando la richiesta ritorna un risultato
-		if(xhr.readyState ==4) {	// se è andata a buon fine fa
+		if(xhr.readyState ==4) { // se è andata a buon fine fa
 			console.log(xhr.responseText); // Stampo a console il contenuto della risposta da parte del server
-		
+			var dd = document.createElement("dd"); // creo il dd
+			dd.textContent = "Prodotto aggiunto al carrello!"; // scrivo il testo del messaggio
+			dd.classList.add("successo"); // assegno la classe al dd
+			dt.parentNode.insertBefore(dd, dt.nextSibling); // inserisco il dd
 		}
 		else {
-
+			var dd = document.createElement("dd"); // creo il dd
+			dd.textContent = "Errore nell'aggiunta al carrello!"; // scrivo il testo del messaggio
+			dd.classList.add("errore"); // assegno la classe al dd
+			dt.parentNode.insertBefore(dd, dt.nextSibling); // inserisco il dd
 		}
 	}
 	xhr.open("POST", "take_away.php", true);	// impostazioni tipo di richiesta POST alla pagina take_away.php
