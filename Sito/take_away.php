ceb9977c9fda2f1a69d0cfe9a7e8dd5a5f06a79f
Alessandro <alessandro.dindinelli@gmail.com>
2020-01-15 15:55:57 +0100

    Tolto user di test

--- Sito/take_away.php
+++ Sito/take_away.php
@@ -2,9 +2,6 @@
 	require_once("php/dbaccess.php");
 
 	session_start();
-	#Login per testare la pagina;
-	$_SESSION['username'] = "user"; #da togliere
-	$_SESSION['password'] = "user"; #da togliere
 
 	if (isset($_SESSION['username']) && isset($_POST['action']) && $_POST['action'] == 'aggiungi')
 	{
