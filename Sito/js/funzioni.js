a6f4306c1cc1a403a677349f4ee98bcc6c2149f0
Alessandro <alessandro.dindinelli@gmail.com>
2020-01-18 10:55:02 +0100

    Creata validazioneFormPagamento()

--- Sito/js/funzioni.js
+++ Sito/js/funzioni.js
@@ -458,3 +458,77 @@ function validazioneFormModificaProdotto() {
 	
 	return risPorzione && risPrezzo;
 }
+
+//Funzione per la validazione del form di pagamento
+function validazioneFormPagamento()
+{
+    // Controllo il primo fieldset
+    var nome_cognome = document.getElementById("nome_cognome");
+	var via = document.getElementById("via");
+	var civico = document.getElementById("civico");
+	var cap = document.getElementById("cap");
+	var tel = document.getElementById("tel");
+
+	var risNomeCognome = checkNomeCognome(nome_cognome);
+	var risVia = checkAlfanumericoESpazi(via);
+	var risCivico = checkCivico(civico);
+	var risCAP = checkCAP(cap);
+	var risTel = checkSoloNumerieDim(tel);
+
+	if (!risNomeCognome) {
+	    mostraErrore(nome_cognome, "Il nome deve contenere solo lettere ed essere lungo almeno due caratteri");
+	}
+	if (!risVia) {
+	    mostraErrore(indirizzo,"La via non deve contenere caratteri speciali ed essere lunga almeno due caratteri");
+	}
+	if (!risCivico) {
+	    mostraErrore(civico,"Il numero civico deve essere del formato corretto (e.g. 4, 4b, 4-b o 4/1)");
+	}
+	if (!risCAP) {
+	    mostraErrore(cap,"Il CAP deve essere di Padova e contenere solo numeri");
+	}
+	if (!risTel) {
+	    mostraErrore(tel,"Non hai inserito un numero telefonico corretto");
+	}
+
+    // Controllo il secondo fieldset
+	var intestatario_carta = document.getElementById("intestatario_carta");
+	var num_carta = document.getElementById("num_carta");
+	var mese_scad = document.getElementById("mese_scad");
+	var anno_scad = document.getElementById("anno_scad");
+	var cvv = document.getElementById("cvv_carta");
+
+	var mese_scad_val = mese_scad.options[mese_scad.selectedIndex].text;
+	var anno_scad_value = anno_scad.options[anno_scad.selectedIndex].text;
+
+	var risMeseScad = (mese_scad_val == "Mese");
+	var risAnnoScad = (anno_scad_value == "Anno");
+
+	var risIntestatario = checkNomeCognome(intestatario_carta);
+	var risNum = checkSoloNumerieDim(num_carta);
+	var risCvv = checkSoloNumeri(cvv);
+
+	if (risMeseScad) {
+		togliErrore(mese_scad);
+		mostraErrore(mese_scad, "Seleziona il mese di scadenza");
+	} else {
+		togliErrore(mese_scad);
+	}
+	if (risAnnoScad) {
+		togliErrore(anno_scad);
+		mostraErrore(anno_scad, "Seleziona l'anno di scadenza");
+	} else {
+		togliErrore(anno_scad);
+	}
+
+	if (!risIntestatario) {
+		mostraErrore(intestatario_carta, "L'intestatario deve contenere solo lettere ed essere lungo almeno due caratteri");
+	}
+	if (!risNum) {
+		mostraErrore(num_carta, "Non hai inserito un numero della carta corretto");
+	}
+	if (!risCvv) {
+		mostraErrore(cvv, "Il CVV deve essere composto da tre cifre");
+	}
+    return risNomeCognome && risVia && risCivico && risCAP && risTel && risNumCarta && risIntestatario && !risMeseScad && !risAnnoScad && risCvv;
+}
